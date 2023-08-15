91d9a199a426c1e9449c1cc0bc1390cf5381dc03
Sidnei Hernandes Junior <sidnei.hernandes@treeinova.com.br>
2023-08-14 21:01:21 -0300

    identity configuration

---
+++ src/services/NS.Identity.Api/Data/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NS.Identity.Api.Data
+{
+    public class Context : IdentityDbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+    }
+}
--- src/services/NS.Identity.Api/Program.cs
+++ src/services/NS.Identity.Api/Program.cs
@@ -1,15 +1,24 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using NS.Identity.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<Context>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+            .AddEntityFrameworkStores<Context>()
+            .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -19,6 +28,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllers();
 
